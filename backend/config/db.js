d76ed2e4bcda67ea626dba489b064fcf4ee3a693
Rimon Hossain <rimonhossen333@gmail.com>
2024-08-24 14:53:18 +0600

    second commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://topten3610:yDCJTpSoQYrnxe3e@cluster1.5weez.mongodb.net/ecommerceMernDB"
-    );
+    await mongoose.connect(process.env.MONGODB_ATLAS_URL);
   } catch (err) {
     console.log(err);
   }
